07d8d4a0ba84316a654bfaf5f7f6fa816a2367b1
Administrator <Administrator@windows10.microdone.cn>
2016-07-01 00:02:44 +0800

    mybaitis

--- myproject/src/main/java/com/my/util/DBHelper.java
+++ myproject/src/main/java/com/my/util/DBHelper.java
@@ -1,5 +1,24 @@
 package com.my.util;
 
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 public class DBHelper {
+	
+	/**
+	 * 通过factory 获取sqlsession
+	 * @return
+	 * @throws IOException
+	 */
+	public static SqlSession getSqlSession() throws IOException{
+		Reader reader=Resources.getResourceAsReader("com/my/config/Configuration.xml");
+		SqlSessionFactory factory=new SqlSessionFactoryBuilder().build(reader);
+		return factory.openSession();
+	}
  
 }
